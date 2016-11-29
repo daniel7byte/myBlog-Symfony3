de90ea0281d770c60bf2fe71abf03dbe1488e96c
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2016-11-29 14:59:33 -0500

    bcrypt de contraseñas
    se encriptaron las contraseñas y se hizo unos cambios en el layout y el
    mensaje flash

--- src/BlogBundle/Controller/UserController.php
+++ src/BlogBundle/Controller/UserController.php
@@ -35,7 +35,12 @@ class UserController extends Controller
                 $user->setName($form->get("name")->getData());
                 $user->setSurname($form->get("surname")->getData());
                 $user->setEmail($form->get("email")->getData());
-                $user->setPassword($form->get("password")->getData());
+
+                $factory = $this->get("security.encoder_factory");
+                $encoder = $factory->getEncoder($user);
+                $password = $encoder->encodePassword($form->get("password")->getData(), $user->getSalt());
+
+                $user->setPassword($password );
                 $user->setRole("ROLE_USER");
                 $user->setImagen(null);
 
